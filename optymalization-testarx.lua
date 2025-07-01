9fc7a2a09fa4e1360cdf57300abde9f6a8d2788e
BloxBanter <bloxbanter@gmail.com>
2025-07-02 01:35:45 +0200

    Update optymalization-testarx.lua

--- optymalization-testarx.lua
+++ optymalization-testarx.lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "LixHub - Anime Rangers X",
+   Name = "LixHub - Anime Rangers X - optimization",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
    LoadingTitle = "Loading for Anime Rangers X",
    LoadingSubtitle = "by Lix",
